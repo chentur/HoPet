7a829e3e492f8bfc09ae90712be0877435b42428
yardenl <yardenliron66@gmail.com>
2018-09-14 01:01:19 +0300

    Twitter API

--- HoPet/Controllers/WebServicesController.cs
+++ HoPet/Controllers/WebServicesController.cs
@@ -1,4 +1,5 @@
 ﻿using HoPet.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +10,68 @@ namespace HoPet.Controllers
 {
     public class WebServicesController : Controller
     {
+        WebServices webServices = new WebServices();
+
         // GET: WebServices/Wiki
         public ActionResult Wiki()
         {
-            return PartialView();
+            var sessionUser = ((User)HttpContext.Session["user"]);
+            if (sessionUser != null)
+            {
+                if (!sessionUser.IsAdmin)
+                {
+                    return RedirectToAction("Index", "Error", new { message = "Not autorized!" });
+                }
+                return PartialView();
+            }
+            return RedirectToAction("Index", "Error", new { message = "You have to login first.." });
         }
 
         // GET : WebServices/GetWikiValue/title
         public String GetWikiValue(string title)
         {
-            WebServices webServices = new WebServices();
-            return webServices.GetWikiValue(title);
+            var sessionUser = ((User)HttpContext.Session["user"]);
+            if (sessionUser != null && sessionUser.IsAdmin)
+            {
+                return webServices.GetWikiValue(title);
+            }
+            return null;
+        }
+
+        // GET: WebServices/Twitter
+        public ActionResult Twitter()
+        {
+            var sessionUser = ((User)HttpContext.Session["user"]);
+            if (sessionUser != null)
+            {
+                if (!sessionUser.IsAdmin)
+                {
+                    return RedirectToAction("Index", "Error", new { message = "Not autorized!" });
+                }
+                return View();
+            }
+            return RedirectToAction("Index", "Error", new { message = "You have to login first.." });
+        }
+
+        // GET: WebServices/GetTwitterPosts
+        public string GetTwitterPosts()
+        {
+            var sessionUser = ((User)HttpContext.Session["user"]);
+            if (sessionUser != null && sessionUser.IsAdmin)
+            {
+                return JsonConvert.SerializeObject(webServices.GetTweets());
+            }
+            return null;
+        }
+
+        // GET: WebServices/PostTweet
+        public void PostTweet(string content)
+        {
+            var sessionUser = ((User)HttpContext.Session["user"]);
+            if (sessionUser != null && sessionUser.IsAdmin)
+            {
+                webServices.PostTweet(content);
+            }
         }
     }
 }
--- HoPet/Models/WebServices.cs
+++ HoPet/Models/WebServices.cs
@@ -3,17 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
+using Tweetinvi;
 
 namespace HoPet.Models
 {
     public class WebServices
     {
+        public const string CONSUMER_KEY = "mve2gLEBf1uXhMBRG5R7inWy0";
+        public const string CONSUMER_SECRET = "Xjs4mgfEy9tbP6VuP7SUBgkxlSlSUjI1mtzKIepqNY6X0Qjg4Q";
+        public const string ACCESS_TOKEN = "48067925-AGOwqbGLbZRzTqz0tKuJnyAYFWC2Q3Vy0Fe5XPnVe";
+        public const string ACCESS_TOKEN_SECRET = "In8mlQWQGuapepxbpYZ1mTyVmWm9c4a8gLWCYzugLU8a9";
+
+        private Tweetinvi.Models.IAuthenticatedUser user;
+    
+        public WebServices()
+        {
+            user = GetTwitterUser();
+        }
+
         public String GetWikiValue(string title)
         {
             title = title.Replace(" ", "%20");
             string url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&exintro&titles=" + title + "&redirects=true";
             var client = new WebClient();
             return client.DownloadString(url);
+        }
+
+        public void PostTweet(string content)
+        {
+            Tweet.PublishTweet(content);
+        }
+
+        public Object GetTweets()
+        {
+            return Timeline.GetUserTimeline(user);
+        }
+
+        private Tweetinvi.Models.IAuthenticatedUser GetTwitterUser()
+        {
+            Auth.SetUserCredentials(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+            return Tweetinvi.User.GetAuthenticatedUser();
         }
     }
 }
