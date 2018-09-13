01643fb32cd678f255019b199da96610e4f60352
yardenl <yardenliron66@gmail.com>
2018-09-13 15:22:18 +0300

    Added wiki web service

---
+++ HoPet/Controllers/WebServicesController.cs
@@ -0,0 +1,25 @@
+﻿using HoPet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoPet.Controllers
+{
+    public class WebServicesController : Controller
+    {
+        // GET: WebServices/Wiki
+        public ActionResult Wiki()
+        {
+            return PartialView();
+        }
+
+        // GET : WebServices/GetWikiValue/title
+        public String GetWikiValue(string title)
+        {
+            WebServices webServices = new WebServices();
+            return webServices.GetWikiValue(title);
+        }
+    }
+}
---
+++ HoPet/Models/WebServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace HoPet.Models
+{
+    public class WebServices
+    {
+        public String GetWikiValue(string title)
+        {
+            title = title.Replace(" ", "%20");
+            string url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&exintro&titles=" + title + "&redirects=true";
+            var client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
