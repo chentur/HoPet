a4e0d9da739bc2e4885da121385cae8b9257d44f
Chen Turgeman <chentur0512@gmail.com>
2018-09-15 11:57:52 +0300

    Add organization controller & views

---
+++ HoPet/Controllers/OrganizationsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoPet.Models;
+
+namespace HoPet.Controllers
+{
+    public class OrganizationsController : Controller
+    {
+        private ProjectDBContext db = new ProjectDBContext();
+
+        // GET: Organizations
+        public ActionResult Index()
+        {
+            return View(db.Organizations.ToList());
+        }
+
+        // GET: Organizations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Organization organization = db.Organizations.Find(id);
+            if (organization == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organization);
+        }
+
+        // GET: Organizations/Create
+        public ActionResult Create()
+        {
+            var sessionUser = ((User)HttpContext.Session["user"]);
+            if (sessionUser != null && sessionUser.IsAdmin)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Error", new { message = "Not autorized!" });
+            }
+        }
+
+        // POST: Organizations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,PhoneNumber,Area")] Organization organization)
+        {
+            if (ModelState.IsValid)
+            {
+                if (organization.PhoneNumber.Length != 10)
+                {
+                    ViewBag.ErrMsg = "The phone number need to be 10 digits";
+                }
+                else if (db.Organizations.Where(curr => curr.Name == organization.Name).Count() > 0)
+                {
+                    ViewBag.ErrMsg = "This organization already exists";
+                }
+                else
+                {
+                    db.Organizations.Add(organization);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(organization);
+        }
+
+        // GET: Organizations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User sessionUser = (User)System.Web.HttpContext.Current.Session["user"];
+            if (sessionUser != null && sessionUser.IsAdmin)
+            {
+                Organization organization = db.Organizations.Find(id);
+                if (organization == null)
+                {
+                    return HttpNotFound();
+                }
+                    return View(organization);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Error", new { message = "Not autorized!" });
+            }
+        }
+
+        // POST: Organizations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,PhoneNumber,Area")] Organization organization)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(organization).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(organization);
+        }
+
+        // GET: Organizations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User sessionUser = (User)System.Web.HttpContext.Current.Session["user"];
+            if (sessionUser != null && sessionUser.IsAdmin)
+            {
+                Organization organization = db.Organizations.Find(id);
+                if (organization == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(organization);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Error", new { message = "Not autorized!" });
+            }
+        }
+
+        // POST: Organizations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Organization organization = db.Organizations.Find(id);
+            db.Organizations.Remove(organization);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- HoPet/Models/Organization.cs
+++ HoPet/Models/Organization.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,8 +14,11 @@ namespace HoPet.Models
     public class Organization
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter organization name")]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = "Please enter organization phone number")]
         public string PhoneNumber { get; set; }
         public Area Area { get; set; }
 
